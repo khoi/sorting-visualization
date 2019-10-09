0edfb1671043da4ee13621a07a86f4591095d2ae
Nghia Tran <vinhnghiatran@gmail.com>
2019-10-09 20:35:10 +0700

    Add Shell Sort 🐙 (#4)
    * Add Shell Sort 🐙
    
    * Prettier Shell Sort
    
    * Missing yield

--- algorithms.js
+++ algorithms.js
@@ -195,3 +195,19 @@ function isSorted(values) {
   }
   return true;
 }
+
+function* shellSort(values) {
+  let n = values.length;
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = gap; i < n; i++) {
+      let tmp = values[i];
+      let j = i;
+      for (j = i; j >= gap && values[j - gap] > tmp; j -= gap) {
+        values[j] = values[j - gap];
+        yield;
+      }
+      values[j] = tmp;
+      yield;
+    }
+  }
+}
